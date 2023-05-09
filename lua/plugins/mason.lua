667286dec712841259836bc9a5c4cf2b8cd9c5df
silveste <silveste@mailbox.org>
2023-05-10 00:35:06 +0100

    fix: wrong config files

--- lua/plugins/mason.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      table.insert(opts.ensure_installed, "prettierd")
-      table.insert(opts.ensure_installed, "typescript-language-server")
-    end,
-  },
-}
