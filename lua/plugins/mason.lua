8f0fa43ff20f5fb20de76e3deca6b99f17cc8c34
silveste <silveste@mailbox.org>
2023-05-09 15:03:11 +0100

    fix: wrong LSP name on mason.lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,7 +3,7 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       table.insert(opts.ensure_installed, "prettierd")
-      table.insert(opts.ensure_installed, "tsserver")
+      table.insert(opts.ensure_installed, "typescript-language-server")
     end,
   },
 }
