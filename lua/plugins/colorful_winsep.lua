cd7ba975ac6f339cee5020983c91c7989267922e
silveste <silveste@mailbox.org>
2023-05-10 18:31:43 +0100

    feat: update colorful_winsep settings

---
+++ lua/plugins/colorful_winsep.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "nvim-zh/colorful-winsep.nvim",
+    config = true,
+    event = { "WinNew" },
+    opts = {
+      highlight = {
+        bg = "#2c3441",
+        fg = "#6a7e95",
+      },
+      -- timer refresh rate
+      interval = 30,
+      -- This plugin will not be activated for filetype in the following table.
+      no_exec_files = { "alpha", "TelescopePrompt", "mason", "noice", "neo-tree" },
+      -- Symbols for separator lines, the order: horizontal, vertical, top left, top right, bottom left, bottom right.
+      symbols = { "━", "┃", "┏", "┓", "┗", "┛" },
+      create_event = function()
+        local colorful_winsep = require("colorful-winsep")
+        local win_n = require("colorful-winsep.utils").calculate_number_windows()
+        local has_filetype = require("utils.has_filetype")
+        if win_n == 2 and has_filetype("neo-tree") then
+          colorful_winsep.NvimSeparatorDel()
+        end
+      end,
+    },
+  },
+}
