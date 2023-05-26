6787a549a2b692dd1c32398cc3a89fb8a3afa2c8
silveste <silveste@mailbox.org>
2023-05-26 16:40:24 +0100

    feat: do not copy when using c key map

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,12 +3,14 @@
 -- Add any additional keymaps here
 -- map modes: https://neovim.io/doc/user/map.html#map-modes
 
+-- MODES
 -- Switch to normal
 vim.keymap.set("i", "jj", "<ESC>", { silent = true, desc = "Switch to normal mode" })
 vim.keymap.set("i", "JJ", "<ESC>", { silent = true, desc = "Switch to normal mode" })
 vim.keymap.set("c", "jj", "<C-c>", { desc = "Switch to normal mode" })
 vim.keymap.set("c", "JJ", "<C-c>", { desc = "Switch to normal mode" })
 
+-- NAVIGATION
 -- Moving cursor on insert and command modes
 vim.keymap.set("i", "<M-j>", "<Down>", { silent = true, desc = "Move the cursor down" })
 vim.keymap.set("i", "<M-k>", "<Up>", { silent = true, desc = "Move the cursor up" })
@@ -19,13 +21,15 @@ vim.keymap.set("c", "<M-k>", "<Up>", { desc = "Move the cursor up" })
 vim.keymap.set("c", "<M-h>", "<Left>", { desc = "Move the cursor left" })
 vim.keymap.set("c", "<M-l>", "<Right>", { desc = "Move the cursor right" })
 
--- Buffers
+-- BUFFERS
 local save_all_buffers = require("utils.save_all_buffers")
 vim.keymap.set({ "n" }, "<C-s><C-s>", save_all_buffers, { silent = true, desc = "Save all buffers" })
 vim.keymap.set({ "i" }, "<C-s><C-s>", save_all_buffers, { silent = true, desc = "Save all buffers" })
 
 vim.keymap.set({ "n" }, "<leader>d", ":bdelete<CR>", { silent = true, desc = "Delete window and buffer" })
 
--- utils
+-- UTILS
 local yank_file_path = require("utils.yank_file_path")
 vim.keymap.set({ "n" }, "yp", yank_file_path, { silent = true, desc = "Path of the file" })
+-- Send replaced text to blackhole register (disables cut function)
+vim.keymap.set({ "n", "v" }, "c", '"_c', { silent = true, desc = "Change text" })
