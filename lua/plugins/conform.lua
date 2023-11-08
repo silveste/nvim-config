4b7175b989bdaef2721dfe8a8bc6adeebe954e90
Antonio Fernandez <antonio.fernandez@teckro.com>
2023-11-08 17:16:19 +0000

    fix: Remove non required key on conform custom config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,7 +1,6 @@
 return {
   {
     "stevearc/conform.nvim",
-    optional = true,
     opts = {
       -- Prettier might break some stylelint rules, therefore stylelint fix is executed after
       formatters_by_ft = {
