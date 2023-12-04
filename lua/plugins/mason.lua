f4559705cadc98cf067fe3e510b9cece087999b4
Antonio Fernandez <antonio.fernandez@teckro.com>
2023-12-04 11:23:24 +0000

    feat: include default LSPs

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,6 +2,9 @@ return {
   "williamboman/mason.nvim",
   opts = function(_, opts)
     opts.ensure_installed = opts.ensure_installed or {}
-    vim.list_extend(opts.ensure_installed, { "stylelint", "stylelint-lsp" })
+    vim.list_extend(
+      opts.ensure_installed,
+      { "css-lsp", "cssmodules-language-server", "marksman", "stylelint", "stylelint-lsp" }
+    )
   end,
 }
